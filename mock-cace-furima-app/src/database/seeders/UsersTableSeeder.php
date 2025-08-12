65effd34a85016c97f54b7f31f147e37242591e9
nakagawa-hayato <daiichi.sho.kyo@gmail.com>
2025-08-12 13:26:06 +0900

    seeder追加

---
+++ mock-cace-furima-app/src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => '鈴木一朗',
+                'email' => 'test1@test.jp',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'name' => '鈴木二郎',
+                'email' => 'test2@test.jp',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'name' => '鈴木三郎',
+                'email' => 'test3@test.jp',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 4,
+                'name' => '鈴木四郎',
+                'email' => 'test4@test.jp',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 5,
+                'name' => '鈴木五郎',
+                'email' => 'test5@test.jp',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
